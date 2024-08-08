99f33399f1edc02b84bcf690c770158f679607e1
Aditya kumar <adityac22f@gmail.com>
2024-08-09 00:00:06 +0530

    error handdle

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,5 +1,11 @@
 class ErrorHandler extends Error{
-    constructor(message, code, status){
-        
+    constructor(message,statusCode){
+      super(message);
+      this.statusCode = statusCode;
     }
+}
+
+export const errorMiddleware = (err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message||500;
 }
